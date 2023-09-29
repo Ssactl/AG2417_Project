3d78f57554ccd6b313aad5bc010039f3d8bfde62
ssactl_jianishe <1285092865@qq.com>
2023-09-29 11:43:41 +0200

    init the layout of game page using flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 //玩游戏的页面，放大地图，小地图和人物展示框这三个组件
 function App() {
   return (
-    <Container fluid>
-      <Col>Big Map</Col>
-      <Col>
-        <Row>Player Display</Row>
-        <Row>Small Map</Row>
-      </Col>
-    </Container>
+    // <Container className="container" fluid="true">
+    //   <Row>
+    //     <Col xs sm md lg xl xxl className="column--1">
+    //       Big Map
+    //     </Col>
+    //     <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4} className="column--2">
+    //       <Row xs={6} sm={6} md={6} lg={6} xl={6} xxl={6} className="row--1">
+    //         Player Display
+    //       </Row>
+    //       <Row className="row--2">Small Map</Row>
+    //     </Col>
+    //   </Row>
+    // </Container>
+
+    <div className="container">
+      <div className="column--1">big map</div>
+      <div className="column--2">
+        <div className="row--1">player display</div>
+        <div className="row--2">small map</div>
+      </div>
+    </div>
   );
 }
 
