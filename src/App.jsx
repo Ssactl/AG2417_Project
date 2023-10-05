5938faa6d0818448351b74afca5f678bf9deb2aa
jues-Wang <53960803+jues-Wang@users.noreply.github.com>
2023-10-05 17:23:35 +0200

    players data from postgresql

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
+import axios from 'axios';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import BigMap from "./components/BigMap/BIgMap";
 import Dice from "./components/BigMap/Dice";
 import PlayerBoard from "./components/PlayerBoard/PlayerBoard";
 import SmallMap from "./components/SmallMap/SmallMap";
+
 
 //for testing
 const stationLocationArr = [
@@ -15,29 +19,30 @@ const stationLocationArr = [
 ];
 const stationCount = stationLocationArr.length;
 
-// // Testing;
-const players = [
-  {
-    name: "Jiani1",
-    avatar: "../src/assets/player/player1.png", // 头像图片的URL
-    score: 1000,
-  },
-  {
-    name: "Lee2",
-    avatar: "../src/assets/player/player2.png",
-    score: 850,
-  },
-  {
-    name: "Meme3",
-    avatar: "../src/assets/player/player3.png",
-    score: 1200,
-  },
-  {
-    name: "Viva4",
-    avatar: "../src/assets/player/player4.png",
-    score: 750,
-  },
-];
+// // // Testing;
+// const players = [
+//   {
+//     name: "Jiani1",
+//     avatar: "../src/assets/player/player1.png", // 头像图片的URL
+//     score: 1000,
+//   },
+//   {
+//     name: "Lee2",
+//     avatar: "../src/assets/player/player2.png",
+//     score: 850,
+//   },
+//   {
+//     name: "Meme3",
+//     avatar: "../src/assets/player/player3.png",
+//     score: 1200,
+//   },
+//   {
+//     name: "Viva4",
+//     avatar: "../src/assets/player/player4.png",
+//     score: 750,
+//   },
+// ];
+
 
 //玩游戏的页面，放大地图，小地图和人物展示框这三个组件
 function App() {
@@ -61,18 +66,22 @@ function App() {
   //current player
   const [currentPlayer, setCurrentPlayer] = useState(0);
 
-  // // 定义用于存储玩家数据的状态
-  // const [players, setPlayers] = useState([]);
-
-  // // 使用 useEffect 来获取玩家数据并更新状态
-  // useEffect(() => {
-  //   fetch("/api/players") // 向后端路由发送请求
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setPlayers(data); // Update state with player data
-  //     })
-  //     .catch((error) => console.error("Error fetching players", error));
-  // }, []); // 空数组表示仅在组件加载时执行一次
+  // frech players data from postgresql
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    // 发起GET请求获取数据
+    axios.get('http://localhost:5000/player/get_players')
+      .then((response) => {
+        // 从响应中提取数据
+        const data = response.data;
+        console.log(data[0].avater)
+        // 更新状态变量以存储数据
+        setPlayers(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // 空数组表示仅在组件挂载时执行一次
 
   return (
     // <Container className="container" fluid="true">
